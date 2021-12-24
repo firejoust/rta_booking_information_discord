1ace9f1d119cc0cf6d72a0b9ceb47149abc91699
firejoust <0gr8tnop@duck.com>
2021-12-24 14:26:01 +1100

    implement working headless solution for selenium

--- scrape_availability.py
+++ scrape_availability.py
@@ -1,83 +1,90 @@
 import sys
-import time
+import asyncio
 import json
 from datetime import datetime
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchWindowException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support.expected_conditions import *
 
 def formatAjaxResult(response):
     timeslots = []
-    for slot in response['result']['ajaxresult']['slots']['listTimeSlot']:
-        print(slot)
-        # timeslot available
-        if slot.slotNumber != 'null':
-            timeslots.append(slot.startTime)
+    
+    for slot in response['ajaxresult']['slots']['listTimeSlot']:
+        if slot['availability']:
+            timeslots.append(slot['startTime'])
     return timeslots
 
 async def retrieveAvailableSlots():
     results = []
     settings = json.load(open("settings.json"))
-    driver = webdriver.Chrome()
+    # assign headless driver for site navigation
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    # create the driver with timeout management
+    driver = webdriver.Chrome(chrome_options=options)
+    wait = WebDriverWait(driver, settings['wait_timer'])
 
     # Attempt login with license credentials
-    driver.get("https://www.myrta.com/wps/myportal/extvp/myrta/")
-    driver.find_element(By.ID, "widget_input_familyName").send_keys(settings['family_name'])
-    driver.find_element(By.ID,"widget_rms_noLogin-input-NSWnumbPCN").send_keys(settings['photo_card_number'])
-    driver.find_element(By.ID,"widget_input_cardNoID").send_keys(settings['card_id_number'])
-    time.sleep(2)
-    driver.find_element(By.ID,"submitNoLogin").click()
+    driver.get("https://www.myrta.com/wps/portal/extvp/myrta/licence/tbs/tbs-login/")
+    wait.until(element_to_be_clickable((By.ID, "widget_input_familyName"))).send_keys(settings['family_name'])
+    wait.until(element_to_be_clickable((By.ID,"widget_rms_noLogin-input-NSWnumbPCN"))).send_keys(settings['photo_card_number'])
+    wait.until(element_to_be_clickable((By.ID,"widget_input_cardNoID"))).send_keys(settings['card_id_number'])
+    wait.until(element_to_be_clickable((By.ID,"submitNoLogin"))).click()
 
     # Save time if a booking has been already made
     if(settings['have_booking']):
         driver.find_element(By.XPATH,'//*[text()="Manage booking"]').click()
-        driver.find_element(By.ID,"changeLocationButton").click()
-        driver.find_element(By.ID,"rms_batLocLoc").click()
-        time.sleep(2)
+        wait.until(element_to_be_clickable((By.ID, "changeLocationButton"))).click()
+        wait.until(element_to_be_clickable((By.ID, "rms_batLocLoc"))).click()
     else:
-        driver.find_element(By.XPATH,'//*[text()="Book test"]').click()
-        driver.find_element(By.ID,"CAR").click()
-        time.sleep(settings['wait_timer'])
-        driver.find_element(By.ID,"DC").click()
-        time.sleep(2)
-        driver.find_element(By.ID,"nextButton").click()
-        driver.find_element(By.ID,"checkTerms").click()
-        time.sleep(2)
-        driver.find_element(By.ID,"nextButton").click()
-        driver.find_element(By.ID,"rms_batLocLocSel").click()
-        time.sleep(2)
+        wait.until(element_to_be_clickable((By.ID,"CAR"))).click()
+        wait.until(element_to_be_clickable((By.ID,"c1tt3"))).click()
+        await asyncio.sleep(2)
+        wait.until(element_to_be_clickable((By.ID, "nextButton"))).click()
+        wait.until(element_to_be_clickable((By.ID, 'checkTerms'))).click()
+        await asyncio.sleep(2)
+        wait.until(element_to_be_clickable((By.ID, "nextButton"))).click()
+        wait.until(element_to_be_clickable((By.ID, "rms_batLocLocSel"))).click()
 
     # Determine which centers should be scanned for available timeslots
     if(settings['centres']):
         options = settings['centres']
         options.insert(0,"")
     else:
-        select_box_first = driver.find_element(By.ID,"rms_batLocationSelect2")
+        select_box_first = wait.until(element_to_be_clickable((By.ID, "rms_batLocationSelect2")))
         options = [x for x in select_box_first.find_elements(By.TAG_NAME,"option")]
         options = [x.get_attribute("value") for x in options]
 
     # Retrieve all available timeslots for each center
     for option in options[1:]:
-        time.sleep(2)
-        driver.find_element(By.ID,"rms_batLocLocSel").click()
-        time.sleep(2)
-        select_box = driver.find_element(By.ID,"rms_batLocationSelect2")
+        # select centre location in dropdown
+        wait.until(element_to_be_clickable((By.ID, "rms_batLocLocSel"))).click()
+        select_box = wait.until(element_to_be_clickable((By.ID, "rms_batLocationSelect2")))
         Select(select_box).select_by_value(option)
-        time.sleep(2)
-        driver.find_element(By.ID,"nextButton").click()
-        if(driver.find_element(By.ID,"getEarliestTime").size!=0):
-            if(driver.find_element(By.ID,"getEarliestTime").is_displayed()):
-                if(driver.find_element(By.ID,"getEarliestTime").is_enabled()):
-                    driver.find_element(By.ID,"getEarliestTime").click()
-
-        response = json.dumps(driver.execute_script('return timeslots'))
+        # goto booking page to find timeslots
+        await asyncio.sleep(2)
+        wait.until(element_to_be_clickable((By.ID, "nextButton"))).click()
+        response = driver.execute_script('return timeslots')
         result = {}
         result['location'] = option
         result['timeslots'] = formatAjaxResult(response)
-        results.push(result)
-
-        # Go to next center
-        driver.find_element(By.ID,"anotherLocationLink").click()
+        results.append(result)
+        # Go to next centre
+        wait.until(element_to_be_clickable((By.ID, "anotherLocationLink"))).click()
 
     driver.quit()
     return results
+
+async def getTimeslots():
+    try:
+        results = await retrieveAvailableSlots()
+        return results
+    except TimeoutException:
+        print("ERROR: Timed out whilst accessing ServiceNSW site. If this continues, try increasing wait_time in settings.json.")
+    except NoSuchWindowException:
+        print("ERROR: The webdriver process was terminated whilst accessing ServiceNSW.")
+    return None
+
+
--- start_client.py
+++ start_client.py
@@ -1,18 +1,24 @@
+import json
 import discord
 import asyncio
-import json
-from scrape_availability import retrieveAvailableSlots
+from scrape_availability import getTimeslots
 
+# configuration
+settings = json.load(open('settings.json'))
+centres = json.load(open('centres.json'))
+history = []
+# globals
 client = discord.Client()
 channel = None
-settings = None
 active = True
 
+# login event
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print('Make sure to register your message channel with "$register" first!')
 
+# message event
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -20,37 +26,71 @@ async def on_message(message):
 
     if message.content.startswith('$register'):
         global channel
+        channel = message.channel
         await message.channel.send('Channel registered! Available timeslots will be sent here.')
         print('Channel', channel, 'has been registered.')
-        channel = message.channel
 
     elif message.content.startswith('$stop'):
         global active
+        active = False
         await message.channel.send('Shutting down!')
         await client.close()
         print('Client "{0.user}" has been deactivated via $stop command.'.format(client))
-        active = False
+
+# filter re-used timeslots
+def refactorTimeslots(location, timeslots):
+    refactored = []
+    # determine unique timeslots
+    for timeslot in timeslots:
+        uuid = location + "@" + timeslot
+        # append unique timeslot to refactored list
+        if not uuid in history:
+            refactored.append(timeslot)
+    return refactored
+
+# add new timeslots to history
+def addHistoryTimeslots(location, timeslots):
+    global history
+    for timeslot in timeslots:
+        uuid = location + "@" + timeslot
+        history.append(uuid)
+
+# inform user of new timeslots
+async def announceTimeslots(results):
+    message = ""
+    # append new timeslots to discord message
+    for centre in results:
+        timeslots = refactorTimeslots(centre['location'], centre['timeslots'])
+
+        # centre has available timeslots
+        if len(timeslots) > 0:
+            centre_name = centres[centre['location']]
+            message += f"\n\n{centre_name}:"
+
+            # concatenate new timeslots to the message
+            for timeslot in timeslots:
+                message += f"\n- {timeslot}"
+
+        # avoid announcing duplicate timeslots
+        addHistoryTimeslots(centre['location'], timeslots)
+
+    # non-blank message indicates new timeslots have been found
+    if message != '':
+        await channel.send('Timeslots are available!' + message)
 
 async def update():
     while active:
-        if channel != None:
-            message = ""
-            results = await retrieveAvailableSlots()
-
-            # append new timeslots to discord message
-            for center in results:
-                if len(center.timeslots) > 0:
-                    message += f"{center.location}: {len(center.timeslots)} time(s) available!\n"
-
-            if message != "":
-                await channel.send(message)
-
         await asyncio.sleep(settings['refresh_timer'])
+        # verify discord channel is registered
+        if channel is None:
+            continue
+        results = await getTimeslots()
+        # check that no issues were encountered whilst retrieving timeslots
+        if results is None:
+            continue
+        await announceTimeslots(results)
 
 def init():
-    global settings
-    settings = json.load(open('settings.json'))
-
     loop = asyncio.get_event_loop()
     loop.create_task(update())
     loop.create_task(client.run(settings['token']))
